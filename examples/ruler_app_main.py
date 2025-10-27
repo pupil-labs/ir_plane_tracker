f19c676692ee87cdceaa109e1d68cf404bcdba3a
Marc Tonsen <mar@pupil-labs.com>
2025-10-27 13:17:48 +0100

    Added instruction on closing the ruler app

--- examples/ruler_app_main.py
+++ examples/ruler_app_main.py
@@ -117,7 +117,7 @@ class RulerWindow(QMainWindow):
         mm_per_pixel_y = height_mm / height_px
 
         label = QLabel(
-            f"{width_px} x {height_px} px\n{width_mm:.1f} x {height_mm:.1f} mm"
+            f"{width_px} x {height_px} px\n{width_mm:.1f} x {height_mm:.1f} mm\nESC to close"
         )
         label.setAlignment(Qt.AlignCenter)
         label.setStyleSheet("color: black; font-size: 24px; background: transparent;")
